2001bd2a8a84311f4fb8c68870a5ffc514339a32
缄默 <pengyouak@163.com>
2023-04-20 11:14:48 +0800

    LBP

--- GUI4OpenCV/Helpers/FeatureHelper.cs
+++ GUI4OpenCV/Helpers/FeatureHelper.cs
@@ -13,7 +13,22 @@ namespace GUI4OpenCV.Helpers
     {
         public static Bitmap LBP(Bitmap bitmap)
         {
-            return default;
+            using var src = bitmap.ToMat();
+
+            // 计算LBP特征
+            using Mat lbpImage = new Mat();
+            Cv2.Laplacian(src, lbpImage, MatType.CV_8U, 1, 1, 0, BorderTypes.Default);
+            Cv2.Threshold(lbpImage, lbpImage, 128, 255, ThresholdTypes.Binary);
+
+            // 显示LBP图像
+            return lbpImage.ToBitmap();
+
+            //// 提取LBP特征向量
+            // Mat lbpHist = new Mat();
+            //Cv2.CalcHist(new Mat[] { lbpImage }, new int[] { 0 }, null, lbpHist, 1, new int[] { 256 }, new Rangef[] { new Rangef(0, 256) });
+
+            //// 归一化特征向量
+            //lbpHist = lbpHist / (src.Rows * src.Cols);
         }
     }
 }
